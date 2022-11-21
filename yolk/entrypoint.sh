097fc1214a39a053f0271d0e9f5bf4dc97fefc3b
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2022-11-21 12:09:46 +0300

    Update entrypoint.sh

--- yolk/entrypoint.sh
+++ yolk/entrypoint.sh
@@ -1,34 +1,5 @@
 #!/bin/bash
 
-#
-# Copyright (c) 2022 incredible-gmod.ru
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-#
-
-# Wait for the container to fully initialize
-sleep 1
-
-# Default the TZ environment variable to UTC.
-TZ=${TZ:-UTC}
-export TZ
-
 # Make internal Docker IP address available to processes.
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}');
 export INTERNAL_IP;
@@ -41,7 +12,7 @@ CLEAR="\033[0m"
 
 if [ "${APT_PACKAGES}" != "" ]; then
 	echo -e "${GREEN}Installing apt packages...${CLEAR}";
-	apt install -y ${APT_PACKAGES};
+	apt install -y "${APT_PACKAGES}";
 fi
 
 mkdir -p /home/container/deps
@@ -52,7 +23,7 @@ mkdir -p /home/container/deps
 #fi
 
 ## Auto update
-if [ -z ${AUTO_UPDATE} ] || [ "${AUTO_UPDATE}" == "1" ]; then
+if [ -z "${AUTO_UPDATE}" ] || [ "${AUTO_UPDATE}" == "1" ]; then
 	echo -e "${GREEN}Auto-update apt packages...${CLEAR}";
 	apt update;
 
@@ -66,19 +37,19 @@ if [ -z ${AUTO_UPDATE} ] || [ "${AUTO_UPDATE}" == "1" ]; then
 		LUVI_VERSION='lastest';
 	fi
 
-	curl -L https://raw.githubusercontent.com/Be1zebub/lit/master/update-lit.sh | bash /dev/stdin -lit-ver ${LIT_VERSION} -luvi-ver ${LUVI_VERSION};
+	curl -L https://raw.githubusercontent.com/Be1zebub/lit/master/update-lit.sh | bash /dev/stdin -lit-ver "${LIT_VERSION}" -luvi-ver "${LUVI_VERSION}";
 
 	if [ -e "package.lua" ]; then
 		echo -e "${GREEN}Auto-update deps from package.lua...${CLEAR}";
 		./lit install
 	fi
 else
-	echo -e '${GREEN}Auto update is disabled, starting an luvit app...${CLEAR}';
+	echo -e "${GREEN}Auto update is disabled, starting an luvit app...${CLEAR}";
 fi
 
 # Replace Startup Variables
-MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g');
+MODIFIED_STARTUP=$(echo -e "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g');
 echo -e ":/home/container$ ${MODIFIED_STARTUP}";
 
 # Run the Server
-eval ${MODIFIED_STARTUP} 2>> /home/container/error.log;
+eval "${MODIFIED_STARTUP}" 2>> /home/container/error.log;
